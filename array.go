eb1de8747fdb6002da1d0e15c819aed391815632
Siriwat <siriwatjaiyungyuen@gmail.com>
2020-12-19 04:08:48 +0700

    [ADD]

--- array.go
+++ array.go
@@ -8,12 +8,16 @@ func main() {
 	arr[1] = 3
 	arr[2] = 4
 
+	arr2 := [5]int64{1,2,3,5,6} //declare w/ value
+
+	//for
 	for i := 0; i < len(arr); i++ {
 		fmt.Println(arr[i])
 	}
 
+	//for-range
 	for _, v := range arr {
 		fmt.Println(v)
 	}
-
+	fmt.Print(arr)
 }
